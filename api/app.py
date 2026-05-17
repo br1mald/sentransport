b2ec29bbac09181e0e26d1fb00469cda6eb3a697
Ibrahima Sory Diallo <isorydiallo05@gmail.com>
2026-05-17 18:57:50 +0000

    Exercices Lab 4

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -31,6 +31,50 @@ def get_ligne(ligne_id):
     if ligne is None:
         return jsonify({"erreur": "Ligne non trouvee"}), 404
     return jsonify(ligne)
+
+
+@app.route("/arrets", methods=["GET"])
+def get_arrets():
+    arrets = set()
+    for ligne in lignes:
+        arrets.update(arret for arret in ligne["listeArrets"])
+    return jsonify(list(arrets))
+
+
+@app.route("/stats", methods=["GET"])
+def get_stats():
+    line_count = 0
+    arrets = set()
+    max_arrets = lignes[0]
+    for ligne in lignes:
+        line_count += 1
+        arrets.update(arret for arret in ligne["listeArrets"])
+        if len(ligne["listeArrets"]) > len(max_arrets["listeArrets"]):
+            max_arrets = ligne
+
+    stop_count = len(arrets)
+
+    return jsonify(
+        {
+            "nombre_lignes": line_count,
+            "nombre_arrets": stop_count,
+            "ligne_avec_max_arrets": max_arrets["id"],
+        }
+    )
+
+
+@app.route("/lignes/recherche", methods=["GET"])
+def recherche():
+    q = request.args.get("q", "")
+    lignes_list = []
+    for ligne in lignes:
+        if q in ligne["depart"] or q in ligne["arrivee"]:
+            lignes_list.append(ligne)
+
+    if len(lignes_list) > 0:
+        return jsonify(lignes_list)
+
+    return jsonify({"message": "ligne introuvable"})
 
 
 if __name__ == "__main__":
